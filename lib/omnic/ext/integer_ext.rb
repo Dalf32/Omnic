0d943ab9e8b25bc01208b9345bc16a58c8f6d269
Dalf32 <kylepmullins@gmail.com>
2020-05-28 00:17:00 -0400

    Support for displaying negative numbers when formatting currency

--- lib/omnic/ext/integer_ext.rb
+++ lib/omnic/ext/integer_ext.rb
@@ -4,6 +4,7 @@
 
 module IntegerExt
   def format_currency
-    '$' + to_s.reverse.scan(/(\d*\.\d{1,3}|\d{1,3})/).join(',').reverse
+    neg = negative? ? '-' : ''
+    "#{neg}$" + to_s.reverse.scan(/(\d*\.\d{1,3}|\d{1,3})/).join(',').reverse
   end
 end
