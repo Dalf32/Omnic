b1343cee5f68baa8516989efbaa84b35201d8205
Dalf32 <kylepmullins@gmail.com>
2026-07-10 01:57:17 -0400

    Adding convenience require files to ease including handlers

---
+++ lib/omnic/core_handlers.rb
@@ -0,0 +1,3 @@
+require_relative 'handlers/admin_functions_handler'
+require_relative 'handlers/audit_handler'
+require_relative 'handlers/help_handler'
---
+++ lib/omnic/extra_handlers.rb
@@ -0,0 +1,4 @@
+require_relative 'handlers/dice_roll_handler'
+require_relative 'handlers/echo_handler'
+require_relative 'handlers/looking_for_group_handler'
+require_relative 'handlers/reminder_handler'
